b1f0daf18322a21ad68fcf54686ba9bc6899085f
Diana Sushik <dianasushik2000@gmail.com>
2022-08-08 16:02:33 -0700

    commit tree 7634d93af4d1b758fb9140cf77a36f092cd408ab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    return Math.abs(42 - pickupBlock) //Use abs because we want a positive number
+}
+
+function distanceFromHqInFeet(pickupBlock){  // where passanger is getting picked up
+    return distanceFromHqInBlocks(pickupBlock) * 264
+}
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end)* 264 // want positive number but transfered to feet
+
+} 
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000){
+        return (distance - 400) * 0.02
+    } else if (distance < 2500){
+        return 25
+    } else { //no need for description because it is the last option
+    return 'cannot travel that far'
+    }
+}
